e02ae88b5082e40abf7cee2cc0474fbb7f1729dc
Dio Ilham Djatiadi <dio_djatiadi@srwasia.com>
2022-01-23 13:15:04 +0700

    setup @heroicons as main icon package

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { AnnotationIcon } from '@heroicons/react/outline'
 
 const App = () => {
   return (
-    <div className="bg-slate-800">
+    <div className="bg-slate-800 flex gap-5">
+      <AnnotationIcon className="h-5 w-5 text-indigo-400" />
       <p className="text-5xl">halo</p>
     </div>
   )
