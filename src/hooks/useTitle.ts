18da8b931227b26a08e6fa1708d91cea0bc5c41a
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-02-06 19:24:22 +0700

    add(global): useTitle hook for updating doc title & upd index.html title

---
+++ src/hooks/useTitle.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+
+/**
+ * Set title once before component mounted and reset on unmount.
+ *
+ * @param title title to be added
+ * @param override use title as prefix or as a full title instead
+ */
+export const useTitle = (title: string, override: boolean = false): void => {
+  useEffect(() => {
+    document.title = override ? title : `${title} | ${process.env.REACT_APP_SITE_TITLE}`
+
+    return () => {
+      document.title = override ? title : `${title} | ${process.env.REACT_APP_SITE_TITLE}`
+    }
+  }, [])
+}
