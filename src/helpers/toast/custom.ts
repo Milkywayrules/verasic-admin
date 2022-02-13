f6cc9387b6a6eddbe34f53d6515921d52f650860
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-02-13 14:15:47 +0700

    refactor(helper/toast): approach to export toast/index.ts & add custom toast example

--- src/helpers/toast/custom.ts
+++ src/helpers/toast/custom.ts
@@ -1,1 +1,22 @@
-// write your custom toast here, extending the base or extended toast
+import extended from './extended'
+
+const custom = {
+  // write your custom toast here as a method, extending the "base" or "extended" toast
+  // and  it will automagically exported to toast helper and available globally
+
+  /**
+   * Describe your method here
+   */
+  _test_() {},
+
+  /**
+   * Describe your method here
+   *
+   * @param title some explanation about the param
+   */
+  _test2_(title: string) {
+    extended.basic(title)
+  },
+}
+
+export default custom
--- src/helpers/toast/index.ts
+++ src/helpers/toast/index.ts
@@ -1,7 +1,24 @@
 import base from './base'
+import custom from './custom'
 import extended from './extended'
 
-export default {
+const toast = {
+  /**
+   * This is base wrapper for the toast. You probably don't need this.
+   */
   _base: { ...base },
+
+  /**
+   * Extended version from the base wrapper.
+   * You probably want to use this.
+   * Except you want the theme to suit better to your design system, then use custom.
+   */
   ...extended,
+
+  /**
+   * Custom version, tailored to your needs by yourself.
+   */
+  custom,
 }
+
+export default toast
