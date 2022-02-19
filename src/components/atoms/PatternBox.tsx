9fd8a8ff13e87e21b52d945e8f11707feb8257cb
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-02-19 17:37:42 +0700

    add(patternbox): theme support for light and dark

--- src/components/atoms/PatternBox.tsx
+++ src/components/atoms/PatternBox.tsx
@@ -2,19 +2,20 @@ import { FC } from 'react'
 import componentUtils from 'src/helpers/componentUtils'
 
 interface Props {
+  theme?: 'light' | 'dark'
   className?: string
 }
 
 /**
  *
  */
-const PatternBox: FC<Props> = ({ children, className = '' }) => {
+const PatternBox: FC<Props> = ({ children, theme = 'light', className = '' }) => {
   return (
     <div
       className={
         `flex flex-col gap-5 rounded border-2 border-gray-400 bg-gray-100 px-8 py-12 ` + className
       }
-      style={componentUtils.style.bgPatterns.endlessClouds.light}
+      style={componentUtils.style.bgPatterns.endlessClouds[theme]}
     >
       {children}
     </div>
