d394779a085a85af50f7010da29cfcb883c54568
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-02-20 23:26:30 +0700

    refactor(patternbox): now handle overflow-auto

--- src/components/atoms/PatternBox.tsx
+++ src/components/atoms/PatternBox.tsx
@@ -13,7 +13,8 @@ const PatternBox: FC<Props> = ({ children, theme = 'light', className = '' }) =>
   return (
     <div
       className={
-        `flex flex-col gap-5 rounded border-2 border-gray-400 bg-gray-100 px-8 py-12 ` + className
+        `flex flex-col gap-5 overflow-auto rounded border-2 border-gray-400 bg-gray-100 px-8 py-12 ` +
+        className
       }
       style={componentUtils.style.bgPatterns.endlessClouds[theme]}
     >
